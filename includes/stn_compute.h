d955145474f33374d1504e77445b06cc3dea336d
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 19:23:19 -0700

    Track OpenCL deterministic result qualification

--- includes/stn_compute.h
+++ includes/stn_compute.h
@@ -41,6 +41,7 @@ typedef struct stn_compute_provider {
     int kernel_ready;
     int argument_ready;
     int execution_ready;
+    int result_ready;
 } stn_compute_provider;
 
 typedef struct stn_compute_inventory {
