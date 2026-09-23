410340f969109c9738a7f12b7e2db6c097bc3e72
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 19:06:52 -0700

    Test OpenCL program build qualification state

--- tests/test_compute.c
+++ tests/test_compute.c
@@ -40,6 +40,7 @@ stn_compute_status stn_compute_platform_detect(
     inventory->providers[0].buffer_ready = 1;
     inventory->providers[0].transfer_ready = 1;
     inventory->providers[0].program_ready = 1;
+    inventory->providers[0].build_ready = 1;
 
     inventory->providers[1].type =
         STN_COMPUTE_PROVIDER_VULKAN;
@@ -60,6 +61,7 @@ stn_compute_status stn_compute_platform_detect(
     inventory->providers[1].buffer_ready = 0;
     inventory->providers[1].transfer_ready = 0;
     inventory->providers[1].program_ready = 0;
+    inventory->providers[1].build_ready = 0;
 
     return STN_COMPUTE_OK;
 }
@@ -189,6 +191,11 @@ int main(void)
     check(
         inventory.providers[0].program_ready == 1,
         "OpenCL program object readiness preserved"
+    );
+
+    check(
+        inventory.providers[0].build_ready == 1,
+        "OpenCL program build readiness preserved"
     );
 
     check(
