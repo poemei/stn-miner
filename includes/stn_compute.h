247ed62108db4498f136c3d54787eb6c47af439d
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 18:23:07 -0700

    Add compute provider discovery interface

---
+++ includes/stn_compute.h
@@ -0,0 +1,41 @@
+#ifndef STN_COMPUTE_H
+#define STN_COMPUTE_H
+
+#include <stddef.h>
+
+#define STN_COMPUTE_MAX_PROVIDERS 8u
+
+typedef enum stn_compute_status {
+    STN_COMPUTE_OK = 0,
+    STN_COMPUTE_INVALID_ARGUMENT,
+    STN_COMPUTE_PLATFORM_ERROR
+} stn_compute_status;
+
+typedef enum stn_compute_provider_type {
+    STN_COMPUTE_PROVIDER_OPENCL = 0,
+    STN_COMPUTE_PROVIDER_CUDA,
+    STN_COMPUTE_PROVIDER_HIP,
+    STN_COMPUTE_PROVIDER_LEVEL_ZERO,
+    STN_COMPUTE_PROVIDER_VULKAN,
+    STN_COMPUTE_PROVIDER_D3D12
+} stn_compute_provider_type;
+
+typedef struct stn_compute_provider {
+    stn_compute_provider_type type;
+    const char *runtime;
+} stn_compute_provider;
+
+typedef struct stn_compute_inventory {
+    stn_compute_provider providers[STN_COMPUTE_MAX_PROVIDERS];
+    size_t count;
+} stn_compute_inventory;
+
+stn_compute_status stn_compute_detect(
+    stn_compute_inventory *inventory
+);
+
+const char *stn_compute_provider_name(
+    stn_compute_provider_type type
+);
+
+#endif
