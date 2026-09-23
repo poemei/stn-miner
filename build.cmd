625b1deba7770e4ec578dc1c3558e6d2a2a4ea0a
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 18:00:00 -0700

    Build Windows GPU detection

--- build.cmd
+++ build.cmd
@@ -8,11 +8,13 @@ cl /nologo /std:c17 /W4 /O2 ^
     src\main.c ^
     src\stn_config.c ^
     src\stn_display.c ^
+    src\stn_gpu.c ^
     src\stn_log.c ^
     src\stn_miner.c ^
     src\stn_protocol.c ^
     src\stn_hash.c ^
     src\stn_cpu.c ^
+    platforms\windows\stn_gpu_win32.c ^
     platforms\windows\stn_platform_win32.c ^
     /Fe:build\stn-miner.exe ^
     /link ws2_32.lib user32.lib
