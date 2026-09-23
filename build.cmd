079307f870887f8064485ab597a46b47d921f575
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 19:41:39 -0700

    Build Windows GPU mining backend

--- build.cmd
+++ build.cmd
@@ -9,6 +9,7 @@ cl /nologo /std:c17 /W4 /O2 ^
     src\stn_config.c ^
     src\stn_compute.c ^
     src\stn_backend.c ^
+    src\stn_gpu_backend.c ^
     src\stn_display.c ^
     src\stn_gpu.c ^
     src\stn_log.c ^
@@ -17,6 +18,7 @@ cl /nologo /std:c17 /W4 /O2 ^
     src\stn_hash.c ^
     src\stn_cpu.c ^
     platforms\windows\stn_compute_win32.c ^
+    platforms\windows\stn_gpu_backend_win32.c ^
     platforms\windows\stn_gpu_win32.c ^
     platforms\windows\stn_platform_win32.c ^
     /Fe:build\stn-miner.exe ^
