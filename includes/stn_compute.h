04c94dd2df0909ac1f5610e116046a2e1a040382
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 18:32:14 -0700

    Track compute platform qualification

--- includes/stn_compute.h
+++ includes/stn_compute.h
@@ -24,6 +24,8 @@ typedef struct stn_compute_provider {
     stn_compute_provider_type type;
     const char *runtime;
     int api_ready;
+    int platform_ready;
+    size_t platform_count;
 } stn_compute_provider;
 
 typedef struct stn_compute_inventory {
