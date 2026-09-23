d5600a71802829a552fea00ec3beed6119a0d220
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 18:23:10 -0700

    Add compute provider platform interface

---
+++ includes/stn_compute_platform.h
@@ -0,0 +1,10 @@
+#ifndef STN_COMPUTE_PLATFORM_H
+#define STN_COMPUTE_PLATFORM_H
+
+#include "stn_compute.h"
+
+stn_compute_status stn_compute_platform_detect(
+    stn_compute_inventory *inventory
+);
+
+#endif
