5d7a86b3ccca94532ebaaee1e4f03239c2d41734
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 19:27:15 -0700

    Test OpenCL arithmetic vector qualification state

--- tests/test_compute.c
+++ tests/test_compute.c
@@ -45,6 +45,7 @@ stn_compute_status stn_compute_platform_detect(
     inventory->providers[0].argument_ready = 1;
     inventory->providers[0].execution_ready = 1;
     inventory->providers[0].result_ready = 1;
+    inventory->providers[0].vector_ready = 1;
 
     inventory->providers[1].type =
         STN_COMPUTE_PROVIDER_VULKAN;
@@ -70,6 +71,7 @@ stn_compute_status stn_compute_platform_detect(
     inventory->providers[1].argument_ready = 0;
     inventory->providers[1].execution_ready = 0;
     inventory->providers[1].result_ready = 0;
+    inventory->providers[1].vector_ready = 0;
 
     return STN_COMPUTE_OK;
 }
@@ -224,6 +226,11 @@ int main(void)
     check(
         inventory.providers[0].result_ready == 1,
         "OpenCL deterministic result readiness preserved"
+    );
+
+    check(
+        inventory.providers[0].vector_ready == 1,
+        "OpenCL arithmetic vector readiness preserved"
     );
 
     check(
