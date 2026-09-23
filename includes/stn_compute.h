d25303ff7a12e10a1f6886689642f2bff5bf3d25
Stn-Labz, LLC. <pmei@stn-labz.com>
2026-09-22 18:44:39 -0700

    Track OpenCL context qualification

--- includes/stn_compute.h
+++ includes/stn_compute.h
@@ -32,6 +32,7 @@ typedef struct stn_compute_provider {
     int device_identity_ready;
     char device_name[STN_COMPUTE_DEVICE_TEXT_MAX];
     char device_vendor[STN_COMPUTE_DEVICE_TEXT_MAX];
+    int context_ready;
 } stn_compute_provider;
 
 typedef struct stn_compute_inventory {
